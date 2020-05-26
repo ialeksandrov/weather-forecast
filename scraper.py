4957ee9628c2cc121632a9f75b13591cdcbc311a
IAlexandrov <ivan.alexandrov@verint.com>
2020-05-26 10:47:31 +0300

    Adding more information for the weather.

--- scraper.py
+++ scraper.py
@@ -3,14 +3,15 @@ import requests
 from bs4 import BeautifulSoup
 
 # Get the page with hardcoded location Sofia
-page = requests.get("https://weather.com/weather/today/l/ \
+PAGE = requests.get("https://weather.com/weather/today/l/ \
                     3cc6a08e912b5b121b32615df8da9c658a6e469de11b2fa805e0fcb14f8b87da")
 # Extracting data
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(PAGE.content, 'html.parser')
 location = soup.find('h1', class_="h4 today_nowcard-location").text
 temperature = soup.find('div', class_='today_nowcard-temp').text
-phrase = soup.find('div', class_='today_nowcard-phrase').text
+PHRASE = soup.find('div', class_='today_nowcard-phrase').text
 feels_like = soup.find('span', class_='deg-feels').text
+table = soup.find('table')
 degree_sign = "\u00b0"
 
 
@@ -42,9 +43,20 @@ def temperature_convertor(temperature):
     return temperature
 
 
-if page.status_code == 200:
+if PAGE.status_code == 200:
     print("Location:{}".format(location_formatter(location)))
-    print("Temperature: {}{} {}".format(round(temperature_convertor(temperature)), degree_sign, phrase))
+    print("Temperature: {}{} {}".format(round(temperature_convertor(temperature)), degree_sign, PHRASE))
     print("Feels like: {}{}".format(round(temperature_convertor(feels_like)), degree_sign))
+    output_rows = []
+    for table_row in table.findAll('tr'):
+        columns = table_row.findAll('td')
+        headers = table_row.findAll('th')
+        output_row = []
+        for column in columns:
+            for header in headers:
+                output_row.append(header.text)
+            output_row.append(column.text)
+        output_rows.append(output_row)
+        print(output_row)
 else:
     print("ERROR: No weather forecast available!")
