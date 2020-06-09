75b6f7caec2cbf0b3fbb3c1f5a14eb541cf894eb
Ivan Aleksandrov <just_a_dream1990@abv.bg>
2020-06-09 23:11:20 +0300

    Rework the code due to the site changes

--- scraper.py
+++ scraper.py
@@ -7,12 +7,10 @@ PAGE = requests.get("https://weather.com/weather/today/l/ \
                     3cc6a08e912b5b121b32615df8da9c658a6e469de11b2fa805e0fcb14f8b87da")
 # Extracting data
 soup = BeautifulSoup(PAGE.content, 'html.parser')
-location = soup.find('h1', class_="h4 today_nowcard-location").text
-temperature = soup.find('div', class_='today_nowcard-temp').text
-PHRASE = soup.find('div', class_='today_nowcard-phrase').text
-feels_like = soup.find('span', class_='deg-feels').text
-table = soup.find('table')
-degree_sign = "\u00b0"
+location = soup.find('h1', class_="_-_-components-src-organism-CurrentConditions-CurrentConditions--location--1YWj_").text
+temperature = soup.find('div', class_='_-_-components-src-organism-CurrentConditions-CurrentConditions--primary--2DOqs').text
+PHRASE = soup.find('div', class_='_-_-components-src-organism-CurrentConditions-CurrentConditions--phraseValue--mZC_p').text
+
 
 
 def location_formatter(location):
@@ -31,33 +29,8 @@ def location_formatter(location):
     return location
 
 
-def temperature_convertor(temperature):
-    """
-       Simple metric convertor from Fahrenheit to Celsius
-       This function takes:
-       :parameter temperature in Fahrenheit
-       :returns temperature in Celsius
-    """
-    temperature = temperature[:-1]
-    temperature = ((float(temperature) - 32) / 1.8)
-
-    return temperature
-
-
 if PAGE.status_code == 200:
     print("Location:{}".format(location_formatter(location)))
-    print("Temperature: {}{} {}".format(round(temperature_convertor(temperature)), degree_sign, PHRASE))
-    print("Feels like: {}{}".format(round(temperature_convertor(feels_like)), degree_sign))
-    output_rows = []
-    for table_row in table.findAll('tr'):
-        columns = table_row.findAll('td')
-        headers = table_row.findAll('th')
-        output_row = []
-        for column in columns:
-            for header in headers:
-                output_row.append(header.text)
-            output_row.append(column.text)
-        output_rows.append(output_row)
-        print(output_row)
+    print("Temperature: {}".format(temperature))
 else:
     print("ERROR: No weather forecast available!")
