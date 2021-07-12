eae38b9c04e8b230c4831636d342fadbd36aa7df
ialeksandrov <ialeksandrov1990@abv.bg>
2021-07-12 21:32:20 +0300

    Change weather forecast scraping site and adapt the script to work with it.

--- scraper.py
+++ scraper.py
@@ -2,15 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 # Get the page with hardcoded location Sofia
-PAGE = requests.get("https://weather.com/weather/today/l/ \
-                    3cc6a08e912b5b121b32615df8da9c658a6e469de11b2fa805e0fcb14f8b87da")
+PAGE = requests.get("https://www.yr.no/en/forecast/daily-table/2-727011/Bulgaria/Sofia-Capital/Sofia/Sofia")
 # Extracting data
 soup = BeautifulSoup(PAGE.content, 'html.parser')
-location = soup.find('h1', class_="_-_-components-src-organism-CurrentConditions-CurrentConditions--location--1YWj_").text
-temperature = soup.find('div', class_='_-_-components-src-organism-CurrentConditions-CurrentConditions--primary--2DOqs').text
-phrase = soup.find('div', class_='_-_-components-src-organism-CurrentConditions-CurrentConditions--precipValue--2aJSf').text
-feels_like = soup.find('span', class_='_-_-components-src-organism-TodayDetailsCard-TodayDetailsCard--feelsLikeTempValue--2icPt').text
-wind_speed = soup.find('span', class_='_-_-components-src-atom-WeatherData-Wind-Wind--windWrapper--3Ly7c').text
+location = soup.find('span', class_='page-header__location-name').text
+temperature = soup.find('div', class_='now-hero__next-hour-temperature-text').text
+feels_like = soup.find('div', class_='feels-like-text').text
+wind_speed = soup.find('span', class_='wind__container').text
 
 
 def location_formatter(location):
@@ -32,10 +30,9 @@ def location_formatter(location):
 
 if __name__ == "__main__":
     if PAGE.status_code == 200:
-        print("Location:{}".format(location_formatter(location)))
-        print("Temperature: {}".format(temperature))
-        print("Feels like: {}".format(feels_like))
-        print("Additional info: {}".format(phrase))
+        print("Location{}".format(location_formatter(location)))
+        print("Temperature {}".format(temperature))
+        print(feels_like)
         print("Wind: {}".format(wind_speed))
     else:
         print("ERROR: No weather forecast available!")
