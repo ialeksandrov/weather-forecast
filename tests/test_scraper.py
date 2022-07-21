b39f45c6c7e87acba310a909c0c45200204a9a97
ivan <ialeksandrov1990@abv.bg>
2022-07-21 10:30:06 +0300

    remove dummy test

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -5,6 +5,3 @@ def test_get_request_to_the_weather_site():
     page = requests.get("https://www.yr.no/en/forecast/daily-table/2-727011/Bulgaria/Sofia-Capital/Sofia/Sofia")
     assert page.status_code == 200
 
-
-def test_upper():
-    assert 'cognyte'.upper() == 'COGNYTE'
