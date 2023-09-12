d9fa300d40b8155a5193a02dd72b9a473ec4b008
ialeksandrov <ialeksandrov1990@abv.bg>
2023-09-12 09:04:22 +0300

    Remove tests and update modules to python 3.11

--- tests/test_scraper.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-def test_get_request_to_the_weather_site():
-    page = requests.get("https://www.yr.no/en/forecast/daily-table/2-727011/Bulgaria/Sofia-Capital/Sofia/Sofia")
-    assert page.status_code == 200
-
